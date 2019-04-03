464aa17c4a1b07d8083110a9a681b59c4bb19c65
Yonathan Feleke <hiyoni78@gmail.com>
2019-04-03 18:56:42 -0500

    Created project

---
+++ TodoList/TodoList/Controllers/TodoItemsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoList.Controllers
+{
+    public class TodoItemsController : Controller
+    {
+        public IActionResult Items()
+        {
+            return View();
+        }
+    }
+}
