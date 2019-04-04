5e88593535458412ffe709f3ddf31633f226d11e
Yonathan Feleke <hiyoni78@gmail.com>
2019-04-03 22:12:53 -0500

    Added Data layer, Model, and view. Although all need modifications

---
+++ TodoList/TodoList/Data/TodoItemsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Models;
+
+namespace TodoList.Data
+{
+    public class TodoItemsRepository
+    {
+        public TodoItem GetTodoItem()
+        {
+            return null;
+        }
+    }
+}
---
+++ TodoList/TodoList/Models/TodoItem.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class TodoItem
+    {
+        public DateTime Id { get; set; }
+        public string Objective { get; set; }
+        public DateTime CompleteOn { get; set; }
+        public List<SelectListItem> Priority
+        {
+            get
+            {
+                return new List<SelectListItem> {
+                    new SelectListItem { Text = "High", Value = "High" },
+                    new SelectListItem { Text = "Medium", Value = "Medium" },
+                    new SelectListItem { Text = "Low", Value = "Low" }
+                };
+            }
+        }
+        public List<SelectListItem> Status
+        {
+            get
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem { Text = "Completed", Value = "Completed" },
+                    new SelectListItem { Text = "In progress", Value = "In progress" },
+                    new SelectListItem { Text = "Not started", Value = "Not started" },
+                    new SelectListItem { Text = "Suspended", Value = "Suspended" }
+                };
+            }
+        }
+    }
+}
