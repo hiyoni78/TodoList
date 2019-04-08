328dea968896d60b52418e6c85fff540f070b178
Yonathan Feleke <hiyoni78@gmail.com>
2019-04-08 16:21:31 -0500

    Added classes to make it more small screen friendly. Also added font-awesome to the project

--- TodoList/TodoList/Data/Data.cs
+++ TodoList/TodoList/Data/Data.cs
@@ -23,7 +23,7 @@ namespace TodoList.Data
                 (
                     1, 
                     DateTime.Today,
-                    "Finish this website",
+                    "Look for a job",
                     DateTime.Today.AddDays(1),
                     TodoItem.PriorityLevel.High,
                     TodoItem.StatusOption.InProgress
@@ -31,11 +31,38 @@ namespace TodoList.Data
                 new TodoItem
                 (
                     2,
-                    DateTime.Today,
-                    "Continue working on Ecan website",
+                    DateTime.Parse("04-01-2019"),
+                    "Continue working on projects",
+                    DateTime.Today.AddDays(3),
+                    TodoItem.PriorityLevel.Medium,
+                    TodoItem.StatusOption.InProgress
+                ),
+                new TodoItem
+                (
+                    3,
+                    DateTime.Parse("02-15-2019"),
+                    "Start Working out",
+                    DateTime.Today.AddDays(12),
+                    TodoItem.PriorityLevel.Low,
+                    TodoItem.StatusOption.NotStarted
+                ),
+                new TodoItem
+                (
+                    4,
+                    DateTime.Parse("12-24-2018"),
+                    "Start a new course",
                     DateTime.Today.AddDays(3),
                     TodoItem.PriorityLevel.Medium,
                     TodoItem.StatusOption.Suspended
+                ),
+                new TodoItem
+                (
+                    5,
+                    DateTime.Today,
+                    "push updates to git hub",
+                    DateTime.Today,
+                    TodoItem.PriorityLevel.High,
+                    TodoItem.StatusOption.InProgress
                 )
             };
             TodoList = todoList;
